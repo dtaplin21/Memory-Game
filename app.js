c4b798c644bed86d1bd9abc1e14f5e5a42afbb07
dominique Taplin <Dtaplin21@aol.com>
2024-03-24 16:00:46 -0700

    add random function

--- app.js
+++ app.js
@@ -7,5 +7,49 @@ const cardArray = [
         name: 'Cheese Burger',
         img: "images/cheeseburger.png"
     },
-    
+    {
+        name: 'hot dog',
+        img: "images/hotdog.png",
+    },
+    {
+        naem: 'ice cream',
+        img: "images/icecream.png",
+    },
+    {
+        name: 'milkshake',
+        img: "images/milkshake.png",
+    },
+    {
+        name: 'nutella',
+        img: "images/nutella.png",
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'Cheese Burger',
+        img: "images/cheeseburger.png"
+    },
+    {
+        name: 'hot dog',
+        img: "images/hotdog.png",
+    },
+    {
+        naem: 'ice cream',
+        img: "images/icecream.png",
+    },
+    {
+        name: 'milkshake',
+        img: "images/milkshake.png",
+    },
+    {
+        name: 'nutella',
+        img: "images/nutella.png",
+    }
 ]
+
+
+cardArray.sort(() => 0.5 - Math.random())
+
+document.querySelector('#grid')
