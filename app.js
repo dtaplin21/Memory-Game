3d8bf6a4bb37f044f5dced22e439aff1b832d26e
dominique Taplin <Dtaplin21@aol.com>
2024-03-24 16:24:13 -0700

    first css style

--- app.js
+++ app.js
@@ -52,4 +52,15 @@ const cardArray = [
 
 cardArray.sort(() => 0.5 - Math.random())
 
-document.querySelector('#grid')
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard() {
+    for(let i = 0; i < 10; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        gridDisplay.appendChild(card)
+    }
+}
+
+createBoard()
