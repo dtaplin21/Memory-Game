1c6e9b0d7a1d9a520ec3d34969b84701b4113f34
dominique Taplin <Dtaplin21@aol.com>
2024-03-24 16:52:21 -0700

    add card attrubutes

--- app.js
+++ app.js
@@ -53,14 +53,21 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
+const cardsChosen = []
 
 function createBoard() {
-    for(let i = 0; i < 10; i++) {
+    for(let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
         gridDisplay.appendChild(card)
     }
 }
 
 createBoard()
+
+function flipCard() {
+    let cardID = this.getAttribute('data-id')
+   cardsChosen.push(cardArray[cardID].name)
+}
