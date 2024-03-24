b17dad77f553328306ebcf1ec9f32ffcf68b5f3a
dominique Taplin <Dtaplin21@aol.com>
2024-03-24 15:30:46 -0700

    add pictures

---
+++ app.js
@@ -0,0 +1,11 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'Cheese Burger',
+        img: "images/cheeseburger.png"
+    },
+    
+]
