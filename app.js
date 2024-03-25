61d0c0bbd0bbdb1ca999c5884021ac9db1753e71
dominique Taplin <Dtaplin21@aol.com>
2024-03-25 02:31:59 -0700

    end if project

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 let gridDisplay = document.querySelector('#grid')
+const resultDisplay = document.querySelector('#result')
 let cardsChosen = []
 let cardsChosenIds = [];
 let cardsWon = []
@@ -70,27 +71,35 @@ function createBoard() {
 createBoard()
 function checkMatch() {
    const cards = document.querySelectorAll('img') //search for every image in document
-   const optionOneId = cardsChosenIds[0]; 
-   const optionTwoId = cardsChosenIds[1]; 
-
+   let optionOneId = cardsChosenIds[0]; 
+   let optionTwoId = cardsChosenIds[1]; 
+    console.log(cards)
     console.log('Check for match!')
     if(optionOneId === optionTwoId) {
-        console.log('You have clicked the same image!')
+        cards[optioinOneId].setAttribute('src', images/blank.png);
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('You have clicked the same image!')
     }
 
    if(cardsChosen[0] == cardsChosen[1]) {
        alert('You found a match!')
-    cards[optionOneId].setAttribute('src', 'images/swhite.png') //change image to white
-    cards[optionTwoId].setAttribute('src', 'images/swhite.png');
+    cards[optionOneId].setAttribute('src', 'images/white.png') //change image to white
+    cards[optionTwoId].setAttribute('src', 'images/white.png');
     cards[optionOneId].removeEventListener('click', flipCard);
     cards[optionTwoId].removeEventListener('click', flipCard); // remove so you cant click on the same card twice
     cardsWon.push(cardsChosen)
    } else {
-        cards[optionOneId].setAttribute('src', "images/blank.png")
+        cards[optionOneId].setAttribute('src', "images/blank.png")//change image to white if the images dont match
         cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('You did not find a match!')
    }
+   resultDisplay.innerHTML = cardsWon.length
    cardsChosen = [];
    cardsChosenIds = [];
+
+   if(cardsWon.length === cardsArray.length/2) {
+    resultDisplay.innerHTML = "Congradulations you found them all!"
+   }
 }
 
 function flipCard() {
